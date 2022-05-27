01b5fb5e2a20f338c2c466e7395f3e68dfcbcfa3
BrennanStopnik <brennan.stopnik@codeimmersives.com>
2022-05-27 19:08:53 -0400

    Update rigged dice

--- rigged_dice.js
+++ rigged_dice.js
@@ -6,22 +6,11 @@ const prompt = require("prompt-sync")({sigint: true});
 
 let rigged = Number(prompt("Enter the number you would like to rig (between 1 and 6): "));
 
-let roll = Math.ceil(Math.random()*7); //1-7
-
-/*
-//rigged is 6
-1 = 1/7
-2 = 1/7
-3 = 1/7
-4 = 1/7
-5 = 1/7
-6 = 2/7
-
-*/
+let roll = Math.ceil(Math.random()*8); //1-7
 
 console.log("Actual roll: " + roll);
-if (roll === 7){
-    console.log(rigged);
+if (roll === 7 || roll === 8){
+    console.log("Your number is: " + rigged);
 } else {
     console.log(roll);
 }
